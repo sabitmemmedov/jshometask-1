5f1af833128422e38eb9988d1e8fa0db96c819e9
sabitmemmedov <sd83dp1yj@code.edu.az>
2023-10-19 22:37:41 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,193 @@
+// task-1 start /////////////////////////////////////////////////////////////////////
+
+// let a = 3
+// let b = 1
+
+// if(a > b){
+//     console.log("a b'den boyukdur")
+// }else{
+//     console.log("a b'den kicikdir")
+// }
+
+// task-1 end //////////////////////////////////////////////////////////////////
+
+
+// task-2 start /////////////////////////////////////////////////////////////
+
+// let students = [
+
+//     { id: 1, name: "Ali", surname: "Alisoy", age: 20, grade: 95 },
+
+//     { id: 2, name: "Vusala", surname: "Nabiyeva", age: 34, grade: 77 },
+
+//     { id: 3, name: "Aliya", surname: "Gurbanzade", age: 14, grade: 98 },
+
+//     { id: 4, name: "Sabir", surname: "Nuruyev", age: 15, grade: 54 },
+
+//     { id: 5, name: "Mehman", surname: "Heydarov", age: 22, grade: 100 },
+
+//     { id: 6, name: "Sevana", surname: "Mammadli", age: 41, grade: 35 },
+
+//     { id: 7, name: "Ilham", surname: "Babayev", age: 24, grade: 95 },
+
+//     { id: 8, name: "Namiq", surname: "Gulahmedov", age: 20, grade: 95 },
+
+//     { id: 9, name: "Aygun", surname: "Kazimova", age: 24, grade: 100 }
+// ]
+
+// let newArray = []
+
+// for (let i = 0; i < students.length; i++) {
+//     if (i % 2 !== 1) {
+//         newArray.push(students[i])
+//     }
+// }
+// console.log(newArray)
+
+// task-2 end //////////////////////////////////////////////////////////////////
+
+
+// task-3 start ///////////////////////////////////////////////////////////////
+
+// let students = [
+
+//     { id: 1, name: "Ali", surname: "Alisoy", age: 20, grade: 95 },
+
+//     { id: 2, name: "Vusala", surname: "Nabiyeva", age: 34, grade: 77 },
+
+//     { id: 3, name: "Aliya", surname: "Gurbanzade", age: 14, grade: 98 },
+
+//     { id: 4, name: "Sabir", surname: "Nuruyev", age: 15, grade: 54 },
+
+//     { id: 5, name: "Mehman", surname: "Heydarov", age: 22, grade: 100 },
+
+//     { id: 6, name: "Sevana", surname: "Mammadli", age: 41, grade: 35 },
+
+//     { id: 7, name: "Ilham", surname: "Babayev", age: 24, grade: 95 },
+
+//     { id: 8, name: "Namiq", surname: "Gulahmedov", age: 20, grade: 95 },
+
+//     { id: 9, name: "Aygun", surname: "Kazimova", age: 24, grade: 100 }
+
+// ]
+
+// for (i = 0; i < students.length; i++) {
+//     if(students[i].id % 2 ===1){
+//         console.log(students[i].name)
+//     }
+// }
+
+// task-3 end //////////////////////////////////////////////////////////////////
+
+
+// task-4 start ///////////////////////////////////////////////////////////////
+
+// let students = [
+
+//     { id: 1, name: "Ali", surname: "Alisoy", age: 20, grade: 95 },
+
+//     { id: 2, name: "Vusala", surname: "Nabiyeva", age: 34, grade: 77 },
+
+//     { id: 3, name: "Aliya", surname: "Gurbanzade", age: 14, grade: 98 },
+
+//     { id: 4, name: "Sabir", surname: "Nuruyev", age: 15, grade: 54 },
+
+//     { id: 5, name: "Mehman", surname: "Heydarov", age: 22, grade: 100 },
+
+//     { id: 6, name: "Sevana", surname: "Mammadli", age: 41, grade: 35 },
+
+//     { id: 7, name: "Ilham", surname: "Babayev", age: 24, grade: 95 },
+
+//     { id: 8, name: "Namiq", surname: "Gulahmedov", age: 20, grade: 95 },
+
+//     { id: 9, name: "Aygun", surname: "Kazimova", age: 24, grade: 100 }
+
+// ]
+
+// let newArray = []
+
+// for (let i = 0; i < students.length; i++) {
+//     if (students[i].age > 20) {
+//         newArray.push(students[i])
+//     }
+// }
+// console.log(newArray)
+
+// task-4 end /////////////////////////////////////////////////////////////////
+
+
+
+// task-5 start ////////////////////////////////////////////////////////////////
+
+// let students = [
+
+//     { id: 1, name: "Ali", surname: "Alisoy", age: 20, grade: 95 },
+
+//     { id: 2, name: "Vusala", surname: "Nabiyeva", age: 34, grade: 77 },
+
+//     { id: 3, name: "Aliya", surname: "Gurbanzade", age: 14, grade: 98 },
+
+//     { id: 4, name: "Sabir", surname: "Nuruyev", age: 15, grade: 54 },
+
+//     { id: 5, name: "Mehman", surname: "Heydarov", age: 22, grade: 100 },
+
+//     { id: 6, name: "Sevana", surname: "Mammadli", age: 41, grade: 35 },
+
+//     { id: 7, name: "Ilham", surname: "Babayev", age: 24, grade: 95 },
+
+//     { id: 8, name: "Namiq", surname: "Gulahmedov", age: 20, grade: 95 },
+
+//     { id: 9, name: "Aygun", surname: "Kazimova", age: 24, grade: 100 }
+
+// ]
+
+// let newArray = []
+
+// for (let i = 0; i < students.length; i++) {
+//     if (students[i].id % 2 === 0 && students[i].grade > 90) {
+//         newArray.push(students[i])
+//     }
+// }
+// console.log(newArray)
+
+
+// task-5 end////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+// task-6 start ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let students = [
+
+    { id: 1, name: "Ali", surname: "Alisoy", age: 20, grade: 95 },
+
+    { id: 2, name: "Vusala", surname: "Nabiyeva", age: 34, grade: 77 },
+
+    { id: 3, name: "Aliya", surname: "Gurbanzade", age: 14, grade: 98 },
+
+    { id: 4, name: "Sabir", surname: "Nuruyev", age: 15, grade: 54 },
+
+    { id: 5, name: "Mehman", surname: "Heydarov", age: 22, grade: 100 },
+
+    { id: 6, name: "Sevana", surname: "Mammadli", age: 41, grade: 35 },
+
+    { id: 7, name: "Ilham", surname: "Babayev", age: 24, grade: 95 },
+
+    { id: 8, name: "Namiq", surname: "Gulahmedov", age: 20, grade: 95 },
+
+    { id: 9, name: "Aygun", surname: "Kazimova", age: 24, grade: 100 }
+
+]
+
+let newArray = []
+
+for (let i = 0; i < students.length; i++) {
+    if (students[i].grade === 100 || students[i].grade === 95) {
+        newArray.push(students[i].name)
+    }
+}
+console.log(newArray)
+
+
+
+
+// task-6 end ///////////////////////////////////////////////////////////////////////////////////////////////////////
